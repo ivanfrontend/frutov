e896b25fd47ff5f0c56caab95617f0eebc92e50e
eduardtimko <timkoeduard@yandex.ru>
2019-05-31 20:33:36 +0300

    popap start

--- assets/js/main.js
+++ assets/js/main.js
@@ -316,3 +316,25 @@ lMenu();
 // /Липкое меню
 
 })(jQuery);
+
+
+
+$(document).ready(function() {
+    $('.centerMyBtn').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+
+        // When elemened is focused, some mobile browsers in some cases zoom in
+        // It looks not nice, so we disable it:
+        callbacks: {
+            beforeOpen: function() {
+                if($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
+        }
+    });
+});
